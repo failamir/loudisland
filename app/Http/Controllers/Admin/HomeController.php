61a2c7383a555974a35f326ae27cb849c09405e7
Laravel Daily <info@laraveldaily.com>
2022-07-13 14:53:35 +0000

    QuickAdminPanel automatic commit

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -45,40 +45,6 @@ class HomeController
         $chart2 = new LaravelChart($settings2);
 
         $settings3 = [
-            'chart_title'           => 'Pendaftar Baru',
-            'chart_type'            => 'latest_entries',
-            'report_type'           => 'group_by_date',
-            'model'                 => 'App\Models\Pendaftar',
-            'group_by_field'        => 'created_at',
-            'group_by_period'       => 'day',
-            'aggregate_function'    => 'count',
-            'filter_field'          => 'created_at',
-            'filter_days'           => '7',
-            'group_by_field_format' => 'd-m-Y H:i:s',
-            'column_class'          => 'col-md-6',
-            'entries_number'        => '5',
-            'fields'                => [
-                'nama'           => '',
-                'email'          => '',
-                'no_hp'          => '',
-                'payment'        => '',
-                'status_payment' => '',
-            ],
-            'translation_key' => 'pendaftar',
-        ];
-
-        $settings3['data'] = [];
-        if (class_exists($settings3['model'])) {
-            $settings3['data'] = $settings3['model']::latest()
-                ->take($settings3['entries_number'])
-                ->get();
-        }
-
-        if (!array_key_exists('fields', $settings3)) {
-            $settings3['fields'] = [];
-        }
-
-        $settings4 = [
             'chart_title'        => 'Profit',
             'chart_type'         => 'line',
             'report_type'        => 'group_by_string',
@@ -92,8 +58,42 @@ class HomeController
             'translation_key'    => 'event',
         ];
 
-        $chart4 = new LaravelChart($settings4);
+        $chart3 = new LaravelChart($settings3);
 
-        return view('home', compact('chart1', 'chart2', 'chart4', 'settings3'));
+        $settings4 = [
+            'chart_title'           => 'Pendaftar Baru',
+            'chart_type'            => 'latest_entries',
+            'report_type'           => 'group_by_date',
+            'model'                 => 'App\Models\Pendaftar',
+            'group_by_field'        => 'created_at',
+            'group_by_period'       => 'day',
+            'aggregate_function'    => 'count',
+            'filter_field'          => 'created_at',
+            'filter_days'           => '7',
+            'group_by_field_format' => 'd-m-Y H:i:s',
+            'column_class'          => 'col-md-6',
+            'entries_number'        => '10',
+            'fields'                => [
+                'nama'           => '',
+                'no_tiket'       => '',
+                'checkin'        => '',
+                'status_payment' => '',
+                'payment_type'   => '',
+            ],
+            'translation_key' => 'pendaftar',
+        ];
+
+        $settings4['data'] = [];
+        if (class_exists($settings4['model'])) {
+            $settings4['data'] = $settings4['model']::latest()
+                ->take($settings4['entries_number'])
+                ->get();
+        }
+
+        if (!array_key_exists('fields', $settings4)) {
+            $settings4['fields'] = [];
+        }
+
+        return view('home', compact('chart1', 'chart2', 'chart3', 'settings4'));
     }
 }
