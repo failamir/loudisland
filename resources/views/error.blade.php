ade503690b04965bfff048eb1d59956c6083ee6b
failamir <treonstudio@gmail.com>
2025-09-01 05:59:18 +0700

    Update branding to Mandalika KORPRI Fun Night Run 2025
    Changed page titles and event references from 'Loud Island' to 'Mandalika KORPRI Fun Night Run 2025' across error, finish, and frontend layout Blade views for consistent event branding.

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="./css/output.css" rel="stylesheet">
-  <title>Loud island</title>
+  <title>Mandalika KORPRI Fun Night Run 2025</title>
   <style>
     /* .button {
   position: relative;
@@ -44,6 +45,7 @@
 } */
   </style>
 </head>
+
 <body class="h-screen w-screen">
   <div class="-z-20 h-1/2 bg-white w-screen absolute">
     <img class="w-full h-full blur-lg" src="./assets/images/banner.png">
@@ -52,45 +54,46 @@
     <main class="w-1/2 mt-12 drop-shadow-lg bg-white relative">
       <form action="beli" method="post">
         @csrf
-      <!-- HEADER: START -->
-      <div class="flex flex-row w-full bg-gradient-to-br from-orange-100 to-orange-50">
-        <div class="w-3/5 mr-4 border-r-2 border-black border-dashed">
-          <img class="w-full" src="./assets/images/banner.png">
-        </div>
-        <div class="w-2/5 p-4 flex flex-col justify-between">
-          <div>
-            <div class="font-medium">
-              <h5>Sept</h5>
-              <h5>2-3</h5>
+        <!-- HEADER: START -->
+        <div class="flex flex-row w-full bg-gradient-to-br from-orange-100 to-orange-50">
+          <div class="w-3/5 mr-4 border-r-2 border-black border-dashed">
+            <img class="w-full" src="./assets/images/banner.png">
+          </div>
+          <div class="w-2/5 p-4 flex flex-col justify-between">
+            <div>
+              <div class="font-medium">
+                <h5>Sept</h5>
+                <h5>2-3</h5>
+              </div>
+              <div class="mt-6">
+                <h1 class="text-2xl font-medium">Vierratale Andika Mahesa Fiersa Besari 2022</h1>
+                <p>by Mandalika KORPRI Fun Night Run 2025 </p>
+              </div>
             </div>
-            <div class="mt-6">
-              <h1 class="text-2xl font-medium">Vierratale Andika Mahesa Fiersa Besari 2022</h1>
-              <p>by Loud Island </p>
+            <div class="text-right text-xl font-medium">
+              {{-- <p>Rp 210K - 280K</p> --}}
             </div>
           </div>
-          <div class="text-right text-xl font-medium">
-            {{-- <p>Rp 210K - 280K</p> --}}
-          </div>
         </div>
-      </div>
-      <!-- HEADER: END -->
+        <!-- HEADER: END -->
 
-      <!-- TITLE: START -->
-      <div class="py-6 flex items-center flex-col border-x-2 border-b-2">
-        <h1 class="text-2xl font-medium mb-2">
-         Maaf, Terjadi Kesalahan di Server. Mohon hubungi Penyelengara.
-        </h1>
-        {{-- <p>
+        <!-- TITLE: START -->
+        <div class="py-6 flex items-center flex-col border-x-2 border-b-2">
+          <h1 class="text-2xl font-medium mb-2">
+            Maaf, Terjadi Kesalahan di Server. Mohon hubungi Penyelengara.
+          </h1>
+          {{-- <p>
           Fri, Sept 2, 2022 3:00 PM - Sat, Sept 3, 2022 6:00 PM WITA
         </p> --}}
-      </div>
-      <!-- TITLE: END -->
+        </div>
+        <!-- TITLE: END -->
 
-      <!-- CONTENT: START -->
-      
-    </form>
+        <!-- CONTENT: START -->
+
+      </form>
       <!-- CONTENT: END -->
     </main>
   </div>
 </body>
+
 </html>
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Loud Island 2022') }}</title>
+    <title>{{ config('app.name', 'Mandalika KORPRI Fun Night Run 2025 2022') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -36,7 +36,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Loud Island 2022') }}
+                    {{ config('app.name', 'Mandalika KORPRI Fun Night Run 2025 2022') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -47,11 +47,11 @@
                     <ul class="navbar-nav mr-auto">
                         @guest
                         @else
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('frontend.home') }}">
-                                    {{ __('Dashboard') }}
-                                </a>
-                            </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('frontend.home') }}">
+                                {{ __('Dashboard') }}
+                            </a>
+                        </li>
                         @endguest
                     </ul>
 
@@ -59,65 +59,65 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if(Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if(Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                                <a class="dropdown-item" href="{{ route('frontend.profile.index') }}">{{ __('My profile') }}</a>
+
+                                @can('user_management_access')
+                                <a class="dropdown-item disabled" href="#">
+                                    {{ trans('cruds.userManagement.title') }}
+                                </a>
+                                @endcan
+                                @can('permission_access')
+                                <a class="dropdown-item ml-3" href="{{ route('frontend.permissions.index') }}">
+                                    {{ trans('cruds.permission.title') }}
+                                </a>
+                                @endcan
+                                @can('role_access')
+                                <a class="dropdown-item ml-3" href="{{ route('frontend.roles.index') }}">
+                                    {{ trans('cruds.role.title') }}
+                                </a>
+                                @endcan
+                                @can('user_access')
+                                <a class="dropdown-item ml-3" href="{{ route('frontend.users.index') }}">
+                                    {{ trans('cruds.user.title') }}
+                                </a>
+                                @endcan
+                                @can('pendaftar_access')
+                                <a class="dropdown-item" href="{{ route('frontend.pendaftars.index') }}">
+                                    {{ trans('cruds.pendaftar.title') }}
+                                </a>
+                                @endcan
+                                @can('event_access')
+                                <a class="dropdown-item" href="{{ route('frontend.events.index') }}">
+                                    {{ trans('cruds.event.title') }}
+                                </a>
+                                @endcan
+
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-
-                                    <a class="dropdown-item" href="{{ route('frontend.profile.index') }}">{{ __('My profile') }}</a>
-
-                                    @can('user_management_access')
-                                        <a class="dropdown-item disabled" href="#">
-                                            {{ trans('cruds.userManagement.title') }}
-                                        </a>
-                                    @endcan
-                                    @can('permission_access')
-                                        <a class="dropdown-item ml-3" href="{{ route('frontend.permissions.index') }}">
-                                            {{ trans('cruds.permission.title') }}
-                                        </a>
-                                    @endcan
-                                    @can('role_access')
-                                        <a class="dropdown-item ml-3" href="{{ route('frontend.roles.index') }}">
-                                            {{ trans('cruds.role.title') }}
-                                        </a>
-                                    @endcan
-                                    @can('user_access')
-                                        <a class="dropdown-item ml-3" href="{{ route('frontend.users.index') }}">
-                                            {{ trans('cruds.user.title') }}
-                                        </a>
-                                    @endcan
-                                    @can('pendaftar_access')
-                                        <a class="dropdown-item" href="{{ route('frontend.pendaftars.index') }}">
-                                            {{ trans('cruds.pendaftar.title') }}
-                                        </a>
-                                    @endcan
-                                    @can('event_access')
-                                        <a class="dropdown-item" href="{{ route('frontend.events.index') }}">
-                                            {{ trans('cruds.event.title') }}
-                                        </a>
-                                    @endcan
-
-                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
@@ -126,28 +126,28 @@
 
         <main class="py-4">
             @if(session('message'))
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="alert alert-success" role="alert">{{ session('message') }}</div>
-                        </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="alert alert-success" role="alert">{{ session('message') }}</div>
                     </div>
                 </div>
+            </div>
             @endif
             @if($errors->count() > 0)
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="alert alert-danger">
-                                <ul class="list-unstyled mb-0">
-                                    @foreach($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="alert alert-danger">
+                            <ul class="list-unstyled mb-0">
+                                @foreach($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
                         </div>
                     </div>
                 </div>
+            </div>
             @endif
             @yield('content')
         </main>
