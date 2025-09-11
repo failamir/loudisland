d4a9d646b80d5a28f08f0c92ac1f19fc252a2b3d
failamir <38488480+failamir@users.noreply.github.com>
2025-09-11 08:53:34 +0700

    Update menu.config.jsx

--- fe-korpri/src/config/menu.config.jsx
+++ fe-korpri/src/config/menu.config.jsx
@@ -4,7 +4,7 @@ export const MENU_SIDEBAR = [
   { title: 'Transactions', path: '/transactions', icon: 'cheque' },
   // { title: 'Nomor Lari', path: '/nomor-punggung', icon: 'scan-barcode' },
   { title: 'Racepacks', path: '/racepacks', icon: 'gift' },
-  { title: 'Withdrawal', path: '/withdrawal', icon: 'wallet' },
+  // { title: 'Withdrawal', path: '/withdrawal', icon: 'wallet' },
   { title: 'Users', path: '/users', icon: 'users' },
   { title: 'Roles', path: '/roles', icon: 'shield-tick' },
   { title: 'Permissions', path: '/permissions', icon: 'lock' },
